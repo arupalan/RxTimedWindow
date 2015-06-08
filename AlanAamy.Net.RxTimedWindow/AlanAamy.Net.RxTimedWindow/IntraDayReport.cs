eaed3e61d1c648b7a695e02ef8e578690ce6dfb1
arupalan <arup.banerjee@CRM1771.mitenergy.com>
2015-06-08 15:18:26 +0100

    Initial Project

---
+++ AlanAamy.Net.RxTimedWindow/AlanAamy.Net.RxTimedWindow/IntraDayReport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlanAamy.Net.RxTimedWindow
+{
+
+
+    public class IntraDayReport 
+    {
+        public void Run()
+        {
+            ComposeParts();
+        }
+
+        private void ComposeParts()
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add( new DirectoryCatalog(Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location)));
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+    }
+}
---
+++ AlanAamy.Net.RxTimedWindow/AlanAamy.Net.RxTimedWindow/Services/EnergyService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services;
+
+namespace AlanAamy.Net.RxTimedWindow.Services
+{
+    public interface IEnergyService
+    {
+        IObservable<IEnumerable<PowerTrade>> GetTradesObservable();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Export(typeof(IEnergyService))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public sealed class EnergyService : IEnergyService
+    {
+        private static readonly IPowerService powerService = new PowerService();
+
+        public IObservable<IEnumerable<PowerTrade>> GetTradesObservable()
+        {
+            return Observable.Create<IEnumerable<PowerTrade>>(async (observer,token) =>
+            {
+                IEnumerable<PowerTrade> trades;
+                while ((trades = await powerService.GetTradesAsync(DateTime.Now.Date)) != null)
+                 {
+                     if (token.IsCancellationRequested) { return; }
+                     observer.OnNext(trades);
+                 }
+                 observer.OnCompleted();
+            });          
+        }
+    }
+}
