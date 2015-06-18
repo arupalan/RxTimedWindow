2c32cace5eadf522850a49dd36676c47cbeb00ce
arupalan <arup@alanaamy.net>
2015-06-18 07:41:28 +0100

    More code for test

--- AlanAamy.Net.RxTimedWindow/AlanAamy.Net.RxTimedWindow/EnergyService.cs
+++ AlanAamy.Net.RxTimedWindow/AlanAamy.Net.RxTimedWindow/EnergyService.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Services;
 
 namespace AlanAamy.Net.RxTimedWindow
 {
-    public class IEnergyService<T,P>
+    public interface IEnergyService<T>
     {
+        IEnumerable<T> GetTrades(DateTime date);
+        Task<IEnumerable<T>> GetTradesAsync(DateTime date);
+    }
 
+    public class EnergyService : IEnergyService<PowerTrade>
+    {
+        private static readonly IPowerService SvcPowerService = new PowerService();
+
+
+        public IEnumerable<PowerTrade> GetTrades(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PowerTrade>> GetTradesAsync(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
